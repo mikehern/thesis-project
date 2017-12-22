9c3452c8301a1c09fae0f566b66a63171502ea20
Mike Hernandez <mkhern@gmail.com>
2017-12-21 16:10:36 -0800

    Refactored for conciseness. Finished location-specific get

--- client/index.js
+++ client/index.js
@@ -3,6 +3,7 @@ const app = express();
 const moment = require('moment');
 const port = 1337;
 const bodyParser = require('body-parser');
+const axios = require('axios');
 
 const Promise = require('bluebird');
 
@@ -21,12 +22,55 @@ const client = new cassandra.Client({
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+const USER_SERVICE = `javi's ip/route`;
+const AG_SERVICE = `vinoj's ip/route`;
+const EXPERIENCES_SERVICE = `aric's ip/route`;
+
+
+//Helpers
 const TSNow = moment(Date.now()).format('llll');
 const coinFlip = () => {
-  return (Math.floor(Math.random() * 2) == 0) ?'range': 'rating';
+  return (Math.floor(Math.random() * 2) == 0) ? 'reviews' : 'ratings';
+};
+const dbWrite = (experience) => {
+  const {
+    event_type: e_type,
+    experience_id: x_id,
+    experiment_type: ab_type,
+    user_id: u_id
+  } = experience;
+
+  const query = `INSERT into events.user_events(event_timestamp, event_type, experience_id, experiment_type, user_id) values(${Date.now()}, '${e_type}', ${x_id}, '${ab_type}', ${u_id});`;
+
+  client.execute(query)
+    .then(result => console.log('DB was hit with: ', result))
+    .then(result => res.status(200).send(`DB completed your post at ${TSNow}`))
+    .catch(reason => console.error(reason));
 };
 
+const sendABPayload = (cacheReply, ABResult) => {
+  let clientPayload = [];
+  let dbPayload = [];
+  ABResult = ABResult || coinFlip();
+  return new Promise((resolve, reject) => {
+    resolve(cacheReply => {
+      clientPayload = cacheReply.map(el => el.experiment_type = ABResult);
+      return clientPayload;
+    })
+    .then(clientPayload => res.status(200).send(clientPayload))
+    .then(() => {
+      dbPayload = clientPayload.map(el => el.event_type = 'VIEWED');
+      return dbPayload;
+    })
+    .then(dbPayload => {
+      Promise.map(dbPayload, (experience) => {
+        return dbWrite(experience);
+      })
+    });
+  });
+};
 
+//Routes
 app.get('/', (req, res) => {
   res.status(200).send(`User landed on homepage via root route at ${TSNow}`);
   console.log(`Client GET at '/' ${TSNow}`);
@@ -38,52 +82,32 @@ app.post('/', (req, res) => {
 });
 
 app.post('/events', (req, res) => {
-  const {
-    event_type: e_type,
-    experience_id: x_id,
-    experiment_type: ab_type,
-    user_id: u_id 
-  } = req.body;
-  
-
-  //TODO Change db int to largeint
-  const query = `INSERT into events.user_events(event_timestamp, event_type, experience_id, experiment_type, user_id) values(${Date.now()}, '${e_type}', ${x_id}, '${ab_type}', ${u_id});`;
 
   //TODO: after writing to db, chain a promise to add to Aggregators queue
   //http://docs.aws.amazon.com/sdk-for-javascript/v2/developer-guide/sqs-examples-using-queues.html
-  client.execute(query)
-    .then(result => console.log('DB was hit with: ', result))
-    .then(result => res.status(200).send(`DB completed your post at ${TSNow}`))
-    .catch(reason => console.error(reason));
+
+  dbWrite(req.body);
 });
 
 app.get('/experiences', (req, res) => {
   const cacheKey = `${req.query.user}:results`;
-  let clientPayload;
-  let dbPayload;
+  let clientPayload = [];
+  let dbPayload = [];
 
   cache.lpopAsync(cacheKey, 0, 11)
-    //TODO: build helpers for the repetitive code below
-
     .then(reply => {
-      if (reply.length > 0) {
-        clientPayload = reply.map(el => el.experiment_type = coinFlip());
-        res.status(200).send(clientPayload)
-        .then(dbPayload = clientPayload.map(el => el.event_type = 'VIEWED'))
-        .then('TODO: CALL CASSANDRA QUERY HERE');
+      if (reply.length !== 0) {
+        return sendABPayload(reply);
       } else {
-        if (coinFlip() === 'ratings') {
-          cache.lrangeAsync(popularRatings, 0, 11)
-            .then(reply => clientPayload = reply.map(el => el.experiment_type = coinFlip()))
-            .then(res.status(200).send(clientPayload))
-            .then(dbPayload = clientPayload.map(el => el.event_type = 'VIEWED'))
-          .then('TODO: CALL CASSANDRA QUERY HERE');
-        } else {
-          cache.lrangeAsync(popularReviews, 0, 11)
-            .then(reply => clientPayload = reply.map(el => el.experiment_type = coinFlip()))
-            .then(res.status(200).send(clientPayload))
-            .then(dbPayload = clientPayload.map(el => el.event_type = 'VIEWED'))
-            .then('TODO: CALL CASSANDRA QUERY HERE');
+        const ABResult = coinFlip();
+        if (ABResult === 'ratings') {
+          cache
+            .lrangeAsync(popularRatings, 0, 11)
+            .then(reply => sendABPayload(reply))
+        } else if (ABResult === 'reviews') {
+          cache
+            .lrangeAsync(popularReviews, 0, 11)
+            .then(reply => sendABPayload(reply))
         }
       }
     })
@@ -91,24 +115,44 @@ app.get('/experiences', (req, res) => {
 });
       
 app.get('/experiences/:location', (req, res) => {
-  //Location specified
-    //Look up the location:results LIST
-    //If it exists
-      //LRANGE 12 results
-      //Flip a coin, and sort by review or rating
-      //Decorate each obj with the experiment type
-      //Send response to client with these 12
-      //Async send the user_id/location pair to the user_searches queue
-    //If not
-      //Flip a coin
-      //Make a GET to experiences: sortBy, location, batch number
-      //Use results to populate cache
-      //LRANGE 12 from this cache
-      //Decorate each obj with the experiment type
-      //Send response to client with first 12
-      //Async send the user_id/location pair to the user_searches queue
+  const cacheKey = `${req.query.location}:results`;
+  let clientPayload = [];
+  let dbPayload = [];
 
+  cache.lrangeAsync(cacheKey, 0, 11)
+    .then(reply => {
+      if (reply.length !== 0) {
+        return sendABPayload(reply);
+      } else {
+        const ABResult = coinFlip();
+        return axios.get(EXPERIENCES_SERVICE, {
+          params: {
+            location_id: req.query.location,
+            sort_order: ABResult,
+            batch: 1
+          }
+        })
+        //Starting serially... want to test promise.all concurrency later
+        .then(res => {
+          Promise.map(res.data, (experience) => {
+            return cache.lpushAsync(cacheKey, experience);
+          })
+          .then(() => cache.lrangeAsync(cacheKey, 0, 11))
+          .then(reply => sendABPayload(reply, ABResult))
+        })
+      }
+    })
+    .then(() => {
+      const userSearchPayload = {
+        user_id: req.query.user,
+        location_id: req.query.location
+      }
+      return axios.post(USER_SERVICE, userSearchPayload);
+    })
+    .catch(err => console.error(err));
 });
+
+
 
 if (!module.parent) {
   app.listen(port, () => console.log(`Listening on port ${port}`));
